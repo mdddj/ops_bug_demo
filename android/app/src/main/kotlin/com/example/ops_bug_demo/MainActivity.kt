3b26c401cb41048e30fb3426fd1f5b98827c8768
梁典典 <413153189@qq.com>
2023-10-06 18:01:19 +0800

    第一次提交

---
+++ android/app/src/main/kotlin/com/example/ops_bug_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ops_bug_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:loading_more_list_fast/loading_more_list_fast.dart';
+import 'package:scrollview_observer/scrollview_observer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _s = ScrollController();
+  late final _ss = ListObserverController(controller: _s);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 100,
+                width: MediaQuery.of(context).size.width,
+                child: ListViewObserver(
+                  controller: _ss,
+                  child: LoadingMoreList<String>(ListConfig(itemBuilder: (context, item, index) {
+                    return Container(
+                      margin: const EdgeInsets.all(12),
+                      width: 100 ,
+                      height: double.infinity,
+                      color: Colors.grey,
+                      child: Text(item),
+                    );
+                  }, sourceList: _SourceRepository(),scrollDirection: Axis.horizontal,controller: _s)),
+                ),
+              ),
+              ElevatedButton(onPressed: (){
+                _ss.jumpTo(index: 40);
+              }, child: const Text('滚动到 40'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SourceRepository extends LoadingModel<String> {
+  @override
+  Future<bool> loadData([bool isLoadMoreAction = false]) async {
+    addAll(List.generate(100, (index) => "$index"));
+    return true;
+  }
+
+}
