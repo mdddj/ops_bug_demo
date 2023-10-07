a453355e0b8c3a1578d7192e35d1ea043d7f3ed8
梁典典 <413153189@qq.com>
2023-10-07 10:49:09 +0800

    ExtendedRenderSliverList

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loading_more_list_fast/loading_more_list_fast.dart';
 import 'package:scrollview_observer/scrollview_observer.dart';
+import 'package:extended_list/src/rendering/sliver_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,36 +17,41 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _s = ScrollController();
   late final _ss = ListObserverController(controller: _s);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-                width: MediaQuery.of(context).size.width,
-                child: ListViewObserver(
-                  controller: _ss,
-                  customTargetRenderSliverType: (p0) => true,
-                  child: LoadingMoreList<String>(ListConfig(itemBuilder: (context, item, index) {
-                    return Container(
-                      margin: const EdgeInsets.all(12),
-                      width: 100 ,
-                      height: double.infinity,
-                      color: Colors.grey,
-                      child: Text(item),
-                    );
-                  }, sourceList: _SourceRepository(),scrollDirection: Axis.horizontal,controller: _s)),
-                ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              child: ListViewObserver(
+                controller: _ss,
+                customTargetRenderSliverType: (p0) => p0 is ExtendedRenderSliverList,
+                child: LoadingMoreList<String>(ListConfig(
+                    itemBuilder: (context, item, index) {
+                      return Container(
+                        margin: const EdgeInsets.all(12),
+                        width: 100,
+                        height: double.infinity,
+                        color: Colors.grey,
+                        child: Text(item),
+                      );
+                    },
+                    sourceList: _SourceRepository(),
+                    scrollDirection: Axis.horizontal,
+                    controller: _s),onScrollNotification: (notification) => true,),
               ),
-              ElevatedButton(onPressed: (){
-                _ss.jumpTo(index: 40);
-              }, child: const Text('滚动到 40'))
-            ],
-          ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _ss.jumpTo(index: 40);
+                },
+                child: const Text('滚动到 40'))
+          ],
         ),
       ),
     );
@@ -58,5 +64,4 @@ class _SourceRepository extends LoadingModel<String> {
     addAll(List.generate(100, (index) => "$index"));
     return true;
   }
-
 }
